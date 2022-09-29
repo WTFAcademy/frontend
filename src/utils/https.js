4f1e62b4438c9a41b2882c78dbccbb0fab4b08cd
0xdjango <djangoomo@gmail.com>
2022-09-30 01:08:47 +0800

    fix

--- src/utils/https.js
+++ src/utils/https.js
@@ -1,19 +1,20 @@
-
 import axios from 'axios';
 
 import { BASE_URL, SOLIDITY_COURSE_ID } from '@site/src/configs/request';
 
-let request = null;
+let request = axios.create({
+    baseURL: BASE_URL,
+    timeout: 30000,
+});
 
-function createRequest() {
-    request = axios.create({
-        baseURL: BASE_URL,
-        timeout: 1000,
-        headers: {'Authorization': `Bearer ${JSON.parse(localStorage.getItem('supabase.auth.token'))['currentSession']['access_token']}`}
-    });
-}
+request.interceptors.request.use((config) => {
+    const accountInfo = localStorage.getItem('supabase.auth.token');
 
-createRequest();
+    if (accountInfo) {
+        const access_token = JSON.parse(accountInfo)['currentSession']['access_token'];
+        config.headers.Authorization = `Bearer ${access_token}`;
+    }
+    return config;
+})
 
-export const reloadRequest = createRequest;
 export default request;
