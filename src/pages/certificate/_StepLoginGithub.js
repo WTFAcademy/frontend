be2618150ec4b7ad57afc6d71518abd08ab51d9c
chenchongqiang <chongqiangchen@gmail.com>
2022-11-08 22:39:43 +0800

    refactor: update page

--- src/pages/certificate/_StepLoginGithub.js
+++ src/pages/certificate/_StepLoginGithub.js
@@ -11,8 +11,8 @@ const StepLoginGithub = (props) => {
     const {signInWithGithub, data, isLogin} = useAuth();
 
     const handleLogin = () => {
-        // signInWithGithub(true);
-        next();
+        signInWithGithub(true);
+        // next();
     }
 
     useEffect(() => {
