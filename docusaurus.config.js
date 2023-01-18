87c2d492abc9b58afae75b6aa785124f6707523d
Li, Amazing Ang <d0u.gie@hotmail.com>
2023-01-19 00:32:59 +0800

    update test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  themes: ['@docusaurus/theme-live-codeblock'],
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
