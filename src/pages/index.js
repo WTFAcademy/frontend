0e7689180c9048894fefe6980e54c562267e36a4
Django <djangoomo@gmail.com>
2023-02-18 19:19:31 +0800

    update-local

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ export default function Home() {
     const [isRedirect, setIsRedirect] = useState(false);
     const lang = isBrowser ? window.navigator.language || window.navigator.userLanguage : null;
     const langInConfig = siteConfig.i18n.locales.includes(lang);
-    if(langInConfig && (lang != siteConfig.defaultLocale && !isRedirect)){
+    if(langInConfig && (lang != siteConfig.defaultLocale) && !isRedirect){
         setIsRedirect(true);
         return (<Redirect to={useBaseUrl(`${lang}/`)}></Redirect>)
     }
