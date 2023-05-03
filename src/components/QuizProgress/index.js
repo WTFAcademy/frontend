d4cc3982f21ff4a50d8f99bc04de32f05e3d8e99
GlacierLuo <1090490148@qq.com>
2023-05-03 18:06:52 +0800

    Add english version to quiz progress.

--- src/components/QuizProgress/index.js
+++ src/components/QuizProgress/index.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 import get from "lodash/get";
-import {useRequest} from "ahooks";
-import {getLesson} from "../../api/course";
+import { useRequest } from "ahooks";
+import { getLesson } from "../../api/course";
 import Translate from '@docusaurus/Translate';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function QuizProgress(props) {
-    const {courseId, lessonId} = props;
-    const {data} = useRequest(() => getLesson(courseId, lessonId), {
+    const { courseId, lessonId } = props;
+    const { data } = useRequest(() => getLesson(courseId, lessonId), {
         cacheKey: 'lesson-share-' + lessonId,
     });
+    const { i18n } = useDocusaurusContext();
 
     return (
         <div className={styles.quizProgressBox}>
             <div className={styles.quizProgressTime}>
                 <Translate id="component.QuizProgress.time">Time: </Translate>
-                {get(data, 'data.lesson.estimated_time')}
+                {i18n.currentLocale === 'zh' ? get(data, 'data.lesson.estimated_time') : get(data, 'data.lesson.estimated_time')?.replace('分钟', ' minutes')}
             </div>
             <div className={styles.quizProgressScore}>
                 <Translate id="component.QuizProgress.score">Best Score: </Translate>
