ffc69d8459900445dd93601044a30b12b67d1a16
chenchongqiang <chongqiangchen@gmail.com>
2023-06-14 01:02:40 +0800

    fix: fix dark bg

--- src/pages/home/_CourseSection/index.tsx
+++ src/pages/home/_CourseSection/index.tsx
@@ -6,8 +6,8 @@ import {Button} from "@site/src/components/ui/Button";
 const CourseSection = () => {
 
     return (
-        <div className="w-full bg-white pb-[128px] flex justify-center">
-            <div className="flex flex-col items-center bg-white px-8 w-full md:w-[1028px]">
+        <div className="w-full bg-white dark:bg-background pb-[128px] flex justify-center">
+            <div className="flex flex-col items-center px-8 w-full md:w-[1028px]">
                 <div className="text-[32px]">🐝</div>
                 <div className="text-[42px] font-bold font-ubuntu">Courses</div>
                 <div className="text-sm font-normal mb-[70px]">Courses specially designed by developers for developers</div>
