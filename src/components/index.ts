aad47f725cb7d793136b996c315a60a6702b72c8
b00l <wwgeek@163.com>
2023-06-15 23:12:10 +0800

    chore: 添加Icon统一导出

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+export { default as ContributorList } from "./ContributorList";
+export { default as CourseCard } from "./CourseCard";
+export { default as EventCard } from "./EventCard";
+export { default as SponsorCard } from "./SponsorCard";
+export { default as Stepper } from "./ui/Stepper";
+export { default as Step } from "./ui/Stepper/Step";
+export { Button, buttonVariants } from "./ui/Button";
+export { default as ConnectWalletButton } from "./ui/ConnectWalletButton";
+export { default as Spinner } from "./ui/Spinner";
+export { default as Switch } from "./ui/Switch";
+export { Tabs, TabsList, TabsTrigger, TabsContent } from "./ui/Tabs";
+export { default as Tag } from "./ui/Tag";
---
+++ src/components/ui/Stepper/index.tsx
@@ -0,0 +1,30 @@
+import React, { useMemo, ReactNode } from "react";
+
+export const StepperContext = React.createContext(null);
+
+interface Props {
+  children: ReactNode;
+  activeStep: number;
+}
+
+const Stepper = ({ children, activeStep }: Props) => {
+  const contextValue = useMemo(() => ({ activeStep }), [activeStep]);
+
+  const childrenArray = React.Children.toArray(children).filter(Boolean);
+  const steps = childrenArray.map((step, index) => {
+    return React.cloneElement(step, {
+      index,
+      first: index === 0,
+      last: index + 1 === childrenArray.length,
+      ...step.props,
+    });
+  });
+
+  return (
+    <StepperContext.Provider value={contextValue}>
+      <div className="flex flex-col">{steps}</div>
+    </StepperContext.Provider>
+  );
+};
+
+export default Stepper;
