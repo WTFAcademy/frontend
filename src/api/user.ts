8ea303e1578373968d20ee448ae5259c2cd7ffcb
b00l <wwgeek@163.com>
2023-06-27 19:39:39 +0800

    feat: 添加网络请求错误提示功能

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,23 +1,25 @@
 import request from "./request";
-import {TAuthUser} from "@site/src/typings/auth";
-import {TResult} from "@site/src/typings/common";
+import { TAuthUser } from "@site/src/typings/auth";
+import { TResult } from "@site/src/typings/common";
 
 export const getProfile = (token: string) => {
   const headers = {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
   };
   return request.get(`/user`, { headers });
-}
+};
 
 export const updateProfile = (token: string) => {
   const headers = {
-    'Authorization': `Bearer ${token}`,
-    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
   };
   return request.put(`/user`, { headers });
-}
+};
 
 export const getUserInfo = () => {
-    return request.get<unknown, TResult<TAuthUser>>(`/user`).then((res) => res.data);
-}
+  return request
+    .get<unknown, TResult<TAuthUser>>(`/user`, { ignore: true })
+    .then((res) => res.data);
+};
