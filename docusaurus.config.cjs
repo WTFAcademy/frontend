5617d98368adad6f600b50e298864e06ac43826a
Django <djangoomo@gmail.com>
2024-01-21 22:18:31 +0800

    fix: 斜杠问题+语言适配

--- docusaurus.config.cjs
+++ docusaurus.config.cjs
@@ -63,6 +63,7 @@ const config = {
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
+    trailingSlash: false,
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
