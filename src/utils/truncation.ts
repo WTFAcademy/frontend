8f2c4c39c9eac245d7046a3af04e9b8a30ac04c1
brown <brown@xyz.com>
2024-02-06 12:06:00 +0800

    fix: wallet address show the first six and the last four;

--- src/utils/truncation.ts
+++ src/utils/truncation.ts
@@ -1,11 +1,11 @@
 const truncation = (
   value: string = "",
-  prefix = 5,
+  prefix = 6,
   suffix = 4,
   flag = "***",
 ) => {
   const preValue = value.slice(0, prefix);
-  const sufValue = value.slice(value.length - 1 - suffix, value.length - 1);
+  const sufValue = value.slice(value.length - suffix, value.length);
   return `${preValue}${flag}${sufValue}`;
 };
 export default truncation;
