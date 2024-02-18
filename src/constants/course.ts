a18d7c163625e32d980c96d7946ffda728867351
Django <djangoomo@gmail.com>
2024-02-18 23:42:18 +0800

    fix: twitter share link

--- src/constants/course.ts
+++ src/constants/course.ts
@@ -5,8 +5,8 @@ export const COURSE_ID_SOLIDITY_101 = "e1faa656-8c53-45f4-bb4f-950db92bee60"; //
 export const COURSE_ID_SOLIDITY_102 = "d6a7f8b4-dc00-45d4-888b-79a3d16529ca"; // 进阶
 
 export const CourseIdAndSuffixLinkMap = {
-  [COURSE_ID_SOLIDITY_101]: "solidity-start",
-  [COURSE_ID_SOLIDITY_102]: "solidity-advanced",
+  [COURSE_ID_SOLIDITY_101]: "docs/solidity-101",
+  [COURSE_ID_SOLIDITY_102]: "docs/solidity-102",
 };
 
 export const CourseIdAndNameMap = {
