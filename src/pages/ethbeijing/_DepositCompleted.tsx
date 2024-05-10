3023e2da92a1a2c410a98c1218a224dc820d01a0
Django <djangoomo@gmail.com>
2024-05-11 02:25:16 +0800

    feat: update ethbeijing

--- src/pages/ethbeijing/_DepositCompleted.tsx
+++ src/pages/ethbeijing/_DepositCompleted.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Link from "@docusaurus/Link";
 
 const DepositCompleted = ({ txHash }: { txHash?: string }) => {
   return (
-    <div>
-      <h1>Deposit Completed</h1>
-      {txHash}
+    <div className="container">
+      <div className="text-center">
+        <h1 className="text-[50px]">🎉</h1>
+        <p className="relative font-bold text-[24px] mt-6 mb-[60px] leading-[1.3] text-content">Payment Successful<br/>Looking Forward To Seeing You At The Event!</p>
+        <p className="text-[14px] text-content font-[Inter]">Txn</p>
+        {txHash && (
+          <>
+            <Link 
+              to={`https://etherscan.io/tx/${txHash}`} 
+              className="ethbeijing-tx relative mt-3 hover:text-[#2563EB] hover:no-underline font-[Rubik]" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              {txHash}
+            </Link>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/ethbeijing/_NotQualified.tsx
+++ src/pages/ethbeijing/_NotQualified.tsx
@@ -3,9 +3,14 @@ import React from "react";
 const NotQualified = () => {
   return (
     <div className="container">
-      <h1 className="text-center">
-        Sorry, you are not qualified to join this event
-      </h1>
+      <div className="text-center">
+        <div className="inline-flex w-16 h-16">
+          <img className="w-full h-full" src="/img/ethbeijing-hourglass.png" alt="ETH Beijing"/>
+        </div>
+        <p className="relative font-bold text-[24px] mt-6 leading-[1.3] text-content">
+          You Have Not Been Selected Yet <br/>Please Wait
+        </p>
+      </div>
     </div>
   );
 };
