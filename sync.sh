600259fb98f6eef836acda1f2e288f79e12ae33b
chongqiangchen <chongqiangchen@gmail.com>
2024-06-02 01:21:49 +0800

    chore: update sync doc action

--- sync.sh
+++ sync.sh
@@ -2,7 +2,10 @@
 
 # 读取 sync.json 并解析其内容
 CONFIG_FILE="sync.json"
+LOG_START="--------------------------------\nSync started at $(date)\n\n"
 CHANGES=""
+
+echo -e $LOG_START >> changes.txt
 
 jq -c '.[]' $CONFIG_FILE | while read -r repo; do
   SYNC_URL=$(echo $repo | jq -r '.sync')
@@ -24,14 +27,22 @@ jq -c '.[]' $CONFIG_FILE | while read -r repo; do
   # 删除克隆的仓库
   rm -rf $REPO_NAME
 
-  # 记录更改的文件
-  CHANGED_FILES=$(git status --porcelain | awk '{print $2}')
-  if [ -n "$CHANGED_FILES" ]; then
-    CHANGES="--------------------------------\nDate: $(date)\n\nChanges in ${TARGET_PATH}:\n${CHANGED_FILES}\n"
-  fi
-
   # 返回上级目录
   cd - > /dev/null
+
+  # 记录更改的文件
+  CHANGED_FILES=$(git status docs/ --porcelain | awk '{print $2}')
+  echo $CHANGED_FILES
+  if [ -n "$CHANGED_FILES" ]; then
+    CHANGES="Changes in ${TARGET_PATH}:\n${CHANGED_FILES}\n"
+  fi
+
+  echo -e $CHANGES
+
+  # 输出更改的内容到文件
+  if [ -n "$CHANGES" ]; then
+    echo -e $CHANGES >> changes.txt  # 追加到文件中
+  fi
 done
 
 # 添加更改到 git
@@ -39,10 +50,3 @@ git add docs/
 git config --global user.name 'github-actions[bot]'
 git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 git commit -m "Update tutorials" || echo "No changes to commit"
-
-# 输出更改的内容到文件
-if [ -n "$CHANGES" ]; then
-  echo -e $CHANGES >> changes.txt  # 追加到文件中
-else
-  echo "No changes detected." > changes.txt  # 若没有更改，则写入相应信息
-fi
