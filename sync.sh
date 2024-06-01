bdd7d852abdf3b6aace2d0bd0790d42bd184bd4e
chenchongqiang <chongqiangchen@gmail.com>
2024-06-02 00:30:35 +0800

    chore: update sync doc action

--- sync.sh
+++ sync.sh
@@ -29,7 +29,7 @@ jq -c '.[]' $CONFIG_FILE | while read -r repo; do
 done
 
 # 添加更改到 git
-git add frontend/docs/
+git add docs/
 git config --global user.name 'github-actions[bot]'
 git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 git commit -m "Update tutorials" || echo "No changes to commit"
