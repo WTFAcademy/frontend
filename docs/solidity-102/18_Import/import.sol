1d063b0daf686aba4a406b23f645b3146f9f4872
daxiongya <chongqiangchen@gmail.com>
2024-06-25 22:30:47 +0800

    chore: update pkg

--- docs/solidity-102/18_Import/import.sol
+++ docs/solidity-102/18_Import/import.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.21;
 
 // 通过文件相对位置import
 import './Yeye.sol';
 // 通过`全局符号`导入特定的合约
 import {Yeye} from './Yeye.sol';
 // 通过网址引用
-import '@openzeppelin/contracts/utils/Address.sol';
-// 引用oppenzepplin合约
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Address.sol';
+// 引用OpenZeppelin合约
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract Import {
